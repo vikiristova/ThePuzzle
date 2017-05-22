8b32710d56d38e4064e662f6166c4833c0918b6e
vikiristova <viktorijaristova996@yahoo.com>
2017-05-23 01:47:20 +0200

    First commit on puzzle project

---
+++ Slozuvalka/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class formSlozuvalka
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlSlozuvalka = new System.Windows.Forms.Panel();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblKategorija = new System.Windows.Forms.Label();
+            this.rbFamousArtists = new System.Windows.Forms.RadioButton();
+            this.rbFamousPlaces = new System.Windows.Forms.RadioButton();
+            this.pnlSlozuvalka.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlSlozuvalka
+            // 
+            this.pnlSlozuvalka.Controls.Add(this.button9);
+            this.pnlSlozuvalka.Controls.Add(this.button7);
+            this.pnlSlozuvalka.Controls.Add(this.button6);
+            this.pnlSlozuvalka.Controls.Add(this.button5);
+            this.pnlSlozuvalka.Controls.Add(this.button4);
+            this.pnlSlozuvalka.Controls.Add(this.button3);
+            this.pnlSlozuvalka.Controls.Add(this.button2);
+            this.pnlSlozuvalka.Controls.Add(this.button1);
+            this.pnlSlozuvalka.Location = new System.Drawing.Point(278, 12);
+            this.pnlSlozuvalka.Name = "pnlSlozuvalka";
+            this.pnlSlozuvalka.Size = new System.Drawing.Size(270, 270);
+            this.pnlSlozuvalka.TabIndex = 0;
+            this.pnlSlozuvalka.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(180, 0);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(90, 90);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(0, 90);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(90, 90);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(0, 180);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(90, 90);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(90, 180);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(90, 90);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(180, 90);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 90);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(90, 90);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 90);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(90, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 90);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 90);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Enabled = false;
+            this.btnStart.Location = new System.Drawing.Point(278, 313);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(90, 45);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Започни";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblKategorija
+            // 
+            this.lblKategorija.AutoSize = true;
+            this.lblKategorija.Location = new System.Drawing.Point(13, 26);
+            this.lblKategorija.Name = "lblKategorija";
+            this.lblKategorija.Size = new System.Drawing.Size(102, 13);
+            this.lblKategorija.TabIndex = 2;
+            this.lblKategorija.Text = "Избери категорија";
+            // 
+            // rbFamousArtists
+            // 
+            this.rbFamousArtists.AutoSize = true;
+            this.rbFamousArtists.Location = new System.Drawing.Point(13, 53);
+            this.rbFamousArtists.Name = "rbFamousArtists";
+            this.rbFamousArtists.Size = new System.Drawing.Size(205, 17);
+            this.rbFamousArtists.TabIndex = 3;
+            this.rbFamousArtists.TabStop = true;
+            this.rbFamousArtists.Text = "Уметнички дела од познати автори";
+            this.rbFamousArtists.UseVisualStyleBackColor = true;
+            this.rbFamousArtists.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // rbFamousPlaces
+            // 
+            this.rbFamousPlaces.AutoSize = true;
+            this.rbFamousPlaces.Location = new System.Drawing.Point(13, 84);
+            this.rbFamousPlaces.Name = "rbFamousPlaces";
+            this.rbFamousPlaces.Size = new System.Drawing.Size(154, 17);
+            this.rbFamousPlaces.TabIndex = 4;
+            this.rbFamousPlaces.TabStop = true;
+            this.rbFamousPlaces.Text = "Познати места во светот";
+            this.rbFamousPlaces.UseVisualStyleBackColor = true;
+            this.rbFamousPlaces.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // formSlozuvalka
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 370);
+            this.Controls.Add(this.rbFamousPlaces);
+            this.Controls.Add(this.rbFamousArtists);
+            this.Controls.Add(this.lblKategorija);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pnlSlozuvalka);
+            this.Name = "formSlozuvalka";
+            this.Text = "Сложувалка";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlSlozuvalka.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlSlozuvalka;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblKategorija;
+        private System.Windows.Forms.RadioButton rbFamousArtists;
+        private System.Windows.Forms.RadioButton rbFamousPlaces;
+    }
+}
+
---
+++ Slozuvalka/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Collections;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+
+namespace WindowsFormsApplication1
+{
+    public partial class formSlozuvalka : Form
+    {
+        Point point;
+        ArrayList images=new ArrayList();
+        public formSlozuvalka()
+        {
+            point.X=180;
+            point.Y=180;
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            this.Refresh();
+            foreach (Button b in pnlSlozuvalka.Controls) {
+                b.Enabled = true;
+                Image goalImage;
+                if (rbFamousArtists.Checked)
+                {
+                    goalImage = imageChosen("artistsChosen");
+                }
+                else
+                {
+                    goalImage = imageChosen("placesChosen");
+                }
+              //Image goalImage = Image.FromFile("C:\\Users\\user\\Documents\\vanGogh.jpg");
+                cropGoalImage(goalImage,270,270);
+                partialImagesAsButtons(images);
+                Invalidate();
+            }
+        }
+
+        private Image imageChosen(string chosen)
+        {
+            Image goalImage = null;
+            if (chosen.CompareTo("artistsChosen") == 0)
+            {
+                string[] famousPaintings ={"C:\\Users\\user\\Documents\\Visual Studio 2010\\Projects\\Slozuvalka\\images\\The_Persistence_of_Memory.jpg",
+                                          "C:\\Users\\user\\Documents\\Visual Studio 2010\\Projects\\Slozuvalka\\images\\theKiss.jpg",
+                                          "C:\\Users\\user\\Documents\\Visual Studio 2010\\Projects\\Slozuvalka\\images\\MonaLisa.jpg",
+                                          "C:\\Users\\user\\Documents\\Visual Studio 2010\\Projects\\Slozuvalka\\images\\vanGogh.jpg"};
+               
+                var rand = new Random();
+                string image = famousPaintings[rand.Next(famousPaintings.Length)];
+                string famousPaintingImage = image;
+                //Uri uri = new Uri(famousPaintings[rand.Next(famousPaintings.Length)], UriKind.Relative);
+                bool existence = File.Exists(image);
+                if (existence)
+                {
+                    goalImage = Image.FromFile(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), image));
+
+                }
+                //goalImage = Image.FromFile(famousPaintingImage);
+            }
+            else if(chosen=="placesChosen")
+            {
+                string[] Places ={"C:\\Users\\user\\Documents\\Visual Studio 2010\\Projects\\Slozuvalka\\images\\GreatPyramidOfGiza.jpg",
+                    "C:\\Users\\user\\Documents\\Visual Studio 2010\\Projects\\Slozuvalka\\images\\ColosseumRome.jpg",
+                    "C:\\Users\\user\\Documents\\Visual Studio 2010\\Projects\\Slozuvalka\\images\\StatueOfLiberty.jpg",
+                    "C:\\Users\\user\\Documents\\Visual Studio 2010\\Projects\\Slozuvalka\\images\\Pisa.jpg",
+                    "C:\\Users\\user\\Documents\\Visual Studio 2010\\Projects\\Slozuvalka\\images\\EiffelTower.jpg",
+                    "C:\\Users\\user\\Documents\\Visual Studio 2010\\Projects\\Slozuvalka\\images\\BigBenLondon.jpg",
+                    "C:\\Users\\user\\Documents\\Visual Studio 2010\\Projects\\Slozuvalka\\images\\SydneyOperaHouse.jpg",
+                    "C:\\Users\\user\\Documents\\Visual Studio 2010\\Projects\\Slozuvalka\\images\\GoldenGateBridge.jpg"
+
+                                };
+                var rand=new Random();
+                string PlacesImages = Places[rand.Next(Places.Length)];
+                goalImage = Image.FromFile(PlacesImages);
+            }
+            return goalImage;
+        }
+
+        private void partialImagesAsButtons(ArrayList images)
+        {
+           int i=0;
+           int[] array = { 0, 1, 2, 3, 4, 5, 6, 7 };
+           array = shuffle(array);
+           foreach (Button b in pnlSlozuvalka.Controls)
+           {
+               if (i < array.Length)
+               {
+                   b.Image = (Image)images[array[i]];
+                   i++;
+               }
+           }
+        }
+
+        private int[] shuffle(int[] array)
+        {
+            Random random = new Random();
+            array = array.OrderBy(x=>random.Next()).ToArray();
+            return array;
+        }
+        
+        private void cropGoalImage(Image goalImage, int width, int height)
+        {
+            Bitmap bitmapa = new Bitmap(width,height);
+            Graphics g = Graphics.FromImage(bitmapa);
+            g.DrawImage(goalImage, 0, 0, width, height);
+            g.Dispose();
+            int moveRight = 0;
+            int moveDirection = 0;
+            for (int x = 0; x < 8;x++)
+            {
+                Bitmap partialImage = new Bitmap(90,90);
+                for (int i = 0; i < 90;i++ )
+                {
+                    for (int j = 0; j < 90; j++) { 
+                    partialImage.SetPixel(i,j,bitmapa.GetPixel(i+moveRight,j+moveDirection));
+                    }
+                }
+                images.Add(partialImage);
+                moveRight += 90;
+                if (moveRight == 270) {
+                    moveRight = 0;
+                    moveDirection += 90;
+                    
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            moveButton((Button)sender);
+        }
+
+        private void moveButton(Button button)
+        {
+            if (((button.Location.X == point.X - 90 || button.Location.X == point.X + 90)&&button.Location.Y==point.Y)||(button.Location.Y == point.Y - 90 || button.Location.Y == point.Y + 90)&&button.Location.X==point.X)
+            {
+                Point swap = button.Location;
+                button.Location = point;
+
+                point = swap;
+            }
+            if(point.X==180&&point.Y==180)
+            {
+                validationCheck();
+            }
+        }
+
+        private void validationCheck()
+        {
+            int count = 0;
+            int index;
+            foreach(Button button in pnlSlozuvalka.Controls)
+            {
+                index = (button.Location.Y / 90) * 3 + button.Location.X / 90;
+                if(images[index]==button.Image)
+                {
+                    count++;
+                }
+            }
+            if (count == 8)
+            {
+                MessageBox.Show("Погоди го авторот");
+                    
+            }
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            btnStart.Enabled = true;
+            string choice="";
+            if (rbFamousArtists.Checked)
+            {
+                rbFamousPlaces.Enabled = false;
+                choice = "artistsChosen";
+
+            }
+            else
+            {
+                rbFamousArtists.Enabled = false;
+                choice = "placesChosen";
+            }
+            imageChosen(choice);
+        }
+
+    }
+}
